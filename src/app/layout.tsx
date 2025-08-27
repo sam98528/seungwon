d0951c17f763493d15d09546055d2d8ebeda981c
sam98528 <sam98528@naver.com>
2025-08-28 02:30:17 +0900

    Updated Website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Providers from "@/app/provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -46,6 +47,7 @@ export default function RootLayout({
             {"Skip to content"}
           </a>
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
